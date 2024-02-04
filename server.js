beaaf1b586a924854c5943c415d77ab9690b34db
Mohan Vamsi Sajja <msajja@uncc.edu>
2024-02-04 14:29:29 -0500

    Added server, Axios, and ChartJS

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use('/', express.static('public'));
+
+const budget = {
+    myBudget: [
+    {
+        title: 'Power',
+        budget: 20
+    },
+    {
+        title: 'Gas',
+        budget: 15
+    },
+    {
+        title: 'Internet',
+        budget: 15
+    },
+    {
+        title: 'Rent',
+        budget: 375
+    },
+    {
+        title: 'Groceries',
+        budget: 80
+    },
+    {
+        title: 'Toiletries',
+        budget: 20
+    },
+    {
+        title: 'Other',
+        budget: 50
+    },
+]};
+
+app.get('/hello', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.get('/budget', (req, res) => {
+    res.json(budget);
+})
+
+app.listen(port, () => {
+    console.log('Example app listening at http://localhost:${port}')
+});
